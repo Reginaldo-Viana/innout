8e0bcc56d4d9888f6ea3376d4f3c5071c7989f82
ReginaldoVian <75252897+ReginaldoVian@users.noreply.github.com>
2021-05-17 17:32:52 -0300

    Update WorkingHours.php

--- src/models/WorkingHours.php
+++ src/models/WorkingHours.php
@@ -98,10 +98,12 @@ class WorkingHours extends Model {
 
     function getBalance() {
         if(!$this->time1 && !isPastWorkday($this->work_date)) return '';
+        if($this->worked_time == DAILY_TIME) return '-';
 
         $balance = $this->worked_time - DAILY_TIME;
         $balanceString = getTimeStringFromSeconds(abs($balance));
-        return $balanceString;
+        $sign = $this->worked_time >= DAILY_TIME ? '+' : '-';
+        return "{$sign}{$balanceString}";
     }
 
     public static function getMonthlyReport($userId, $date) {
