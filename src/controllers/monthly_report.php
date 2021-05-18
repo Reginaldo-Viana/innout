31d9deccce7668e6d96804a4ebaed22c29243e9e
ReginaldoVian <75252897+ReginaldoVian@users.noreply.github.com>
2021-05-18 10:42:27 -0300

    Update

--- src/controllers/monthly_report.php
+++ src/controllers/monthly_report.php
@@ -5,6 +5,21 @@ requireValidSession();
 $currentDate = new DateTime();
 
 $user =$_SESSION['user'];
+$users = null;
+if($user->is_admin) {
+    $users = User::get();
+}
+
+$selectedPeriod = $_POST['period'] ? $_POST['period'] : $currentDate->format('Y-m');
+$periods = [];
+for($yarDiff = 0; $yarDiff <= 2; $yarDiff++) {
+    $year = date('Y') - $yarDiff;
+    for($month = 12; $month >= 1; $month--){
+        $date = new DateTime("{$year}-{$month}-1");
+        $periods[$date->format('Y-m')] = strftime('%B de %Y', $date->getTimestamp());
+    }
+}
+
 $registries = WorkingHours::getMonthlyReport($user->id, new DateTime());
 
 $report = [];
@@ -37,5 +52,8 @@ $sign = ($sumOfWorkedTime >= $expectedTime) ? '+' : '-';
 loadTemplateView('monthly_report', [
     'report' => $report,
     'sumOfWorkedTime' =>getTimeStringFromSeconds($sumOfWorkedTime),
-    'balance' => "{$sign}{$balance}"
+    'balance' => "{$sign}{$balance}",
+    'selectedPeriod' => $selectedPeriod,
+    'periods' => $periods,
+    'users' => $users,    
 ]);
--- src/views/monthly_report.php
+++ src/views/monthly_report.php
@@ -7,6 +7,30 @@
         );    
     ?>
     <div>
+        <form class="mb-4" action="#" method="post">
+            <div class="input-group">
+                <select name="user" class="form-control" placeholder="Selecione o usuário...">
+                    <option value="">Selecione o usuário</option>
+                    <?php
+                      foreach($users as $user) {
+                          echo "<option value='{$user->id}'>{$user->name}</option>";
+                      }
+                    ?>
+                </select>
+                <select name="period" class="form-control ml-2" placeholder="Selecione o período...">
+                    <?php
+                      foreach($periods as $key => $month) {
+                          $selected = $key === $selectedPeriod ? 'selected' : '';
+                          echo "<option value='{$key}' {$selected}>{$month}</option>";
+                      }
+                    ?>
+                </select>
+                <button class="btn btn-primary ml-2">
+                      <i class="icofont-search"></i>
+                </button>
+            </div>
+        </form>
+
         <table class="table table-bordered table-striped table-hover">
             <thead>
                 <th>Dia</th>
